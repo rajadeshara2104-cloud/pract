8aa609cb0f36a6c7d81a6a45bd8656f9515251b9
rajadeshara2104-cloud <rajadeshara2104@gmail.com>
2026-06-17 08:15:10 +0000

    set

--- main.py
+++ main.py
@@ -4,3 +4,40 @@ c=a+b
 print(c)
 c=a*b
 print(c)
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+  
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+my_tuple = ("apple", "banana", "cherry",)
+my_tuple = add + (mango,)
+print(my_tuple)z
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+thisset.add(mango)
+
+  
+  
---
+++ raj.py
@@ -0,0 +1,17 @@
+set1 = {11,12,13}
+set2 = {11,15,16}
+
+set3= set1 | set2
+print(set3)
+
+set4=set1 - set2
+print(set4)
+
+set5 = set1 & set2
+print(set5)
+
+
+
+
+
+
