ece51ed7f49b2852db0ffbd15212e19c34569b96
rajadeshara2104-cloud <rajadeshara2104@gmail.com>
2026-06-18 14:14:51 +0000

    night

--- new.py
+++ new.py
@@ -163,22 +163,19 @@ else:
     
     
 
-    day = int(input("Enter day number (1-7): "))
-
+day = 4
 match day:
-    case 1:
-        print("Monday")
-    case 2:
-        print("Tuesday")
-    case 3:
-        print("Wednesday")
-    case 4:
-        print("Thursday")
-    case 5:
-        print("Friday")
-    case 6:
-        print("Saturday")
-    case 7:
-        print("Sunday")
-    case _:
-        print("Invalid day number")
+  case 1:
+    print("Monday")
+  case 2:
+    print("Tuesday")
+  case 3:
+    print("Wednesday")
+  case 4:
+    print("Thursday")
+  case 5:
+    print("Friday")
+  case 6:
+    print("Saturday")
+  case 7:
+    print("Sunday")
